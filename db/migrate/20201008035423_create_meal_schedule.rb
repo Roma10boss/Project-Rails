6a29463814dac2bfa56ada12ebe39bd6fcc58256
Roma10boss <Romagustave10@gmail.com>
2020-10-08 00:07:10 -0400

    After Migrations

--- db/migrate/20201008035423_create_meal_schedule.rb
+++ db/migrate/20201008035423_create_meal_schedule.rb
@@ -1,6 +1,11 @@
 class CreateMealSchedule < ActiveRecord::Migration[6.0]
   def change
     create_table :meal_schedules do |t|
+      t.string :eating_time
+      t.string :meal_type
+      t.references :meal, foreign_key: true
+      t.references :meal_plan, foreign_key: true
+      t.timestamps
     end
   end
 end
--- db/migrate/20201008035820_add_side_to_meals.rb
+++ db/migrate/20201008035820_add_side_to_meals.rb
@@ -1,4 +1,5 @@
 class AddSideToMeals < ActiveRecord::Migration[6.0]
   def change
+    add_column :meals, :side, :string
   end
 end
