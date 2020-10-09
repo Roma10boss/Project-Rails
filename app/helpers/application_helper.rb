b4cc991a73dcbbdb9270ed540da121e2d9d3f531
Roma10boss <Romagustave10@gmail.com>
2020-10-08 22:21:15 -0400

    view home error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+      end
+    
+      def logged_in?
+        !!session[:user_id]
+      # !!@current_user
+      end
+    
+      def current_meal
+        @current_meal ||= Meal.find_by_id(params[:meal_id]) if params[:meal_id]
+      end
+    
+      def current_meal_plan
+        @current_meal_plan ||= MealPlan.find_by_id(params[:meal_plan_id]) if params[:meal_plan_id]
+      end
 end
